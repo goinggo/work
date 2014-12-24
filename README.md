647151d0d3a4e26a2c43b9fd744f3f5cac1d3901
William Kennedy <bill@ardanstudios.com>
2014-12-24 14:54:07 -0500

    New package that implements a work pool of go routines using an unbuffered channel.

--- README.md
+++ README.md
@@ -1,4 +1,91 @@
-work
-====
+# Work
 
-Implements a dynamic work pool of goroutines to perform tasks.
+Copyright 2014 Ardan Studios. All rights reserved.  
+Use of this source code is governed by a MIT license that can be found in the LICENSE handle.
+
+Package work uses an unbuffered channel to create a pool of goroutines that can perform work. Stats are maintained on the number of pending and active requests in the pool. Goroutines can be added and removed from the pool during pool operation. This allows for the implementation of a control port to adjust the size of the pool for maximum performance.
+
+Ardan Studios  
+12973 SW 112 ST, Suite 153  
+Miami, FL 33186  
+bill@ardanstudios.com
+
+[Click To View Documentation](http://godoc.org/github.com/goinggo/work)
+
+Sample App
+
+	// This sample program demostrates how to use the work package
+	// to use a pool of goroutines to get work done.
+	package main
+
+	import (
+		"fmt"
+		"sync"
+		"time"
+
+		"github.com/goinggo/work"
+	)
+
+	// names provides a set of names to display.
+	var names = []string{
+		"steve",
+		"bob",
+		"mary",
+		"therese",
+		"jason",
+	}
+
+	// namePrinter provides special support for printing names.
+	type namePrinter struct {
+		name string
+	}
+
+	// Work implements the Worker interface.
+	func (m *namePrinter) Work() {
+		fmt.Println(m.name)
+		time.Sleep(time.Second)
+	}
+
+	// main is the entry point for all Go programs.
+	func main() {
+		// Create a work value with 2 goroutines.
+		w := work.New(2)
+		w.LogStats(100 * time.Millisecond)
+
+		var wg sync.WaitGroup
+		wg.Add(10 * len(names))
+
+		for i := 0; i < 10; i++ {
+			// Iterate over the slice of names.
+			for _, name := range names {
+				// Create a namePrinter and provide the
+				// specfic name.
+				np := namePrinter{
+					name: name,
+				}
+
+				go func() {
+					// Submit the task to be worked on. When Run
+					// returns we know it is being handled.
+					w.Run(&np)
+					wg.Done()
+				}()
+			}
+		}
+
+		for {
+			var c int
+			fmt.Scanf("%d", &c)
+			if c == 0 {
+				break
+			}
+
+			w.Add(c)
+		}
+
+		wg.Wait()
+
+		// Shutdown the work and wait for all existing work
+		// to be completed.
+		w.Shutdown()
+	}
---
+++ work.go
@@ -0,0 +1,159 @@
+// Copyright 2014 Ardan Studios. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE handle.
+
+// Package work manages a pool of goroutines to perform work.
+package work
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+// Worker must be implemented by types that want to use
+// this worker processes.
+type Worker interface {
+	Work()
+}
+
+// Stats contains information about the work pool.
+type Stats struct {
+	Goroutines int64
+	Pending    int64
+	Active     int64
+}
+
+// Work provides a pool of goroutines that can execute any Worker
+// tasks that are submitted.
+type Work struct {
+	tasks    chan Worker    // Unbuffered channel that work is sent into.
+	wg       sync.WaitGroup // Manages the number of goroutines for shutdown.
+	shutdown chan struct{}  // Closed when the Work pool is being shutdown.
+	mutex    sync.Mutex     // Provides synchronization for managing the pool.
+	remove   int            // The number of goroutines to remove.
+	stats    Stats          // Stats about the health of the pool.
+}
+
+// New creates a new Worker.
+func New(goroutines int) *Work {
+	w := Work{
+		tasks:    make(chan Worker),
+		shutdown: make(chan struct{}),
+	}
+
+	w.Add(goroutines)
+
+	return &w
+}
+
+// LogStats display work pool stats on the specified duration.
+func (w *Work) LogStats(d time.Duration) {
+	w.wg.Add(1)
+
+	go func() {
+		for {
+			select {
+			case <-w.shutdown:
+				w.wg.Done()
+				return
+
+			case <-time.After(d):
+				s := w.Stats()
+				fmt.Printf("G[%d] P[%d] A[%d]\n", s.Goroutines, s.Pending, s.Active)
+			}
+		}
+	}()
+}
+
+// Stats returns the current status for the work pool.
+func (w *Work) Stats() Stats {
+	var s Stats
+	s.Goroutines = atomic.LoadInt64(&w.stats.Goroutines)
+	s.Pending = atomic.LoadInt64(&w.stats.Pending)
+	s.Active = atomic.LoadInt64(&w.stats.Active)
+
+	return s
+}
+
+// Add creates goroutines to process work.
+func (w *Work) Add(goroutines int) {
+	if goroutines == 0 {
+		return
+	}
+
+	w.mutex.Lock()
+	{
+		if goroutines > 0 {
+			// We are adding goroutines to the pool.
+			w.wg.Add(goroutines)
+			atomic.AddInt64(&w.stats.Goroutines, int64(goroutines))
+
+			for i := 0; i < goroutines; i++ {
+				go w.work()
+			}
+		} else {
+			// We are removing goroutines from the pool.
+			goroutines = goroutines * -1
+			current := int(atomic.LoadInt64(&w.stats.Goroutines))
+			if goroutines > current {
+				goroutines = current
+			}
+
+			// Set this value so when goroutines are done processing work
+			// they can check to see if they should quit.
+			w.remove = goroutines
+		}
+	}
+	w.mutex.Unlock()
+}
+
+// work performs the users work and keeps stats.
+func (w *Work) work() {
+	// The for range will block until this goroutines is
+	// asked to perform some work.
+	for t := range w.tasks {
+		atomic.AddInt64(&w.stats.Active, 1)
+		t.Work()
+		atomic.AddInt64(&w.stats.Active, -1)
+
+		if w.shouldDie() {
+			break
+		}
+	}
+
+	atomic.AddInt64(&w.stats.Goroutines, -1)
+	w.wg.Done()
+}
+
+// shouldDie checks if there has been a request to remove some
+// goroutines from the pool.
+func (w *Work) shouldDie() bool {
+	d := false
+	w.mutex.Lock()
+	{
+		if w.remove > 0 {
+			w.remove--
+			d = true
+		}
+	}
+	w.mutex.Unlock()
+
+	return d
+}
+
+// Run wait for the goroutine pool to take the work
+// to be executed.
+func (w *Work) Run(work Worker) {
+	atomic.AddInt64(&w.stats.Pending, 1)
+	w.tasks <- work
+	atomic.AddInt64(&w.stats.Pending, -1)
+}
+
+// Shutdown waits for all the workers to finish
+func (w *Work) Shutdown() {
+	close(w.tasks)
+	close(w.shutdown)
+	w.wg.Wait()
+}
