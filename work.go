fceeed334f795664de31330aa2ded7b40678ad6e
William Kennedy <bill@ardanstudios.com>
2014-12-28 13:26:03 -0500

    Removed idle time. Goroutines are cheap and this is un-necessary.

--- work.go
+++ work.go
@@ -22,9 +22,6 @@ const (
 // ErrorInvalidMinRoutines is the error for the invalid minRoutine parameter.
 var ErrorInvalidMinRoutines = errors.New("Invalid minimum number of routines")
 
-// ErrorInvalidIdleTime is the error for the invalid idle time parameter.
-var ErrorInvalidIdleTime = errors.New("Invalid duration for idle time")
-
 // ErrorInvalidStatTime is the error for the invalid stat time parameter.
 var ErrorInvalidStatTime = errors.New("Invalid duration for stat time")
 
@@ -38,7 +35,6 @@ type Worker interface {
 // tasks that are submitted.
 type Work struct {
 	minRoutines int            // Minumum number of routines always in the pool.
-	idleTime    time.Duration  // Time for routines to die on idle.
 	statTime    time.Duration  // Time to display stats.
 	counter     int            // Maintains a running total number of routines ever created.
 	tasks       chan Worker    // Unbuffered channel that work is sent into.
@@ -51,6 +47,93 @@ type Work struct {
 	pending     int64          // Pending number of routines waiting to submit work.
 }
 
+// New creates a new Worker.
+func New(minRoutines int, statTime time.Duration) (*Work, error) {
+	if minRoutines < 0 {
+		return nil, ErrorInvalidMinRoutines
+	}
+
+	if statTime < time.Millisecond {
+		return nil, ErrorInvalidStatTime
+	}
+
+	w := Work{
+		minRoutines: minRoutines,
+		statTime:    statTime,
+		tasks:       make(chan Worker),
+		control:     make(chan int),
+		kill:        make(chan struct{}),
+		shutdown:    make(chan struct{}),
+	}
+
+	// Start the manager.
+	w.manager()
+
+	// Add the routines.
+	w.Add(minRoutines)
+
+	return &w, nil
+}
+
+// Add creates routines to process work or sets a count for
+// routines to terminate.
+func (w *Work) Add(routines int) {
+	if routines == 0 {
+		return
+	}
+
+	cmd := addRoutine
+	if routines < 0 {
+		routines = routines * -1
+		cmd = rmvRoutine
+	}
+
+	for i := 0; i < routines; i++ {
+		w.control <- cmd
+	}
+}
+
+// work performs the users work and keeps stats.
+func (w *Work) work(id int) {
+done:
+	for {
+		select {
+		case t := <-w.tasks:
+			atomic.AddInt64(&w.active, 1)
+			{
+				// Perform the work.
+				t.Work(id)
+			}
+			atomic.AddInt64(&w.active, -1)
+
+		case <-w.kill:
+			break done
+		}
+	}
+
+	// Decrement the counts.
+	atomic.AddInt64(&w.routines, -1)
+	w.wg.Done()
+
+	log.Println("Work : gr : Info : Shutdown")
+}
+
+// Run wait for the goroutine pool to take the work
+// to be executed.
+func (w *Work) Run(work Worker) {
+	atomic.AddInt64(&w.pending, 1)
+	{
+		w.tasks <- work
+	}
+	atomic.AddInt64(&w.pending, -1)
+}
+
+// Shutdown waits for all the workers to finish.
+func (w *Work) Shutdown() {
+	close(w.shutdown)
+	w.wg.Wait()
+}
+
 // manager controls changes to the work pool including stats
 // and shutting down.
 func (w *Work) manager() {
@@ -60,9 +143,7 @@ func (w *Work) manager() {
 		log.Println("Work : manager : Started")
 
 		// Create a timer to run stats.
-		var stats <-chan time.Time
 		timer := time.NewTimer(w.statTime)
-		stats = timer.C
 
 		for {
 			select {
@@ -110,7 +191,7 @@ func (w *Work) manager() {
 					w.kill <- struct{}{}
 				}
 
-			case <-stats:
+			case <-timer.C:
 				// Capture the stats.
 				routines := atomic.LoadInt64(&w.routines)
 				pending := atomic.LoadInt64(&w.pending)
@@ -124,115 +205,4 @@ func (w *Work) manager() {
 			}
 		}
 	}()
-}
-
-// New creates a new Worker.
-func New(minRoutines int, idleTime time.Duration, statTime time.Duration) (*Work, error) {
-	if minRoutines < 0 {
-		return nil, ErrorInvalidMinRoutines
-	}
-
-	if idleTime < time.Minute {
-		return nil, ErrorInvalidIdleTime
-	}
-
-	if statTime < time.Millisecond {
-		return nil, ErrorInvalidStatTime
-	}
-
-	w := Work{
-		minRoutines: minRoutines,
-		idleTime:    idleTime,
-		statTime:    statTime,
-		tasks:       make(chan Worker),
-		control:     make(chan int),
-		kill:        make(chan struct{}),
-		shutdown:    make(chan struct{}),
-	}
-
-	// Start the manager.
-	w.manager()
-
-	// Add the routines.
-	w.Add(minRoutines)
-
-	return &w, nil
-}
-
-// Add creates routines to process work or sets a count for
-// routines to terminate.
-func (w *Work) Add(routines int) {
-	if routines == 0 {
-		return
-	}
-
-	cmd := addRoutine
-	if routines < 0 {
-		routines = routines * -1
-		cmd = rmvRoutine
-	}
-
-	for i := 0; i < routines; i++ {
-		w.control <- cmd
-	}
-}
-
-// work performs the users work and keeps stats.
-func (w *Work) work(id int) {
-	// Create a timer to track idle time.
-	var idle <-chan time.Time
-	var timer *time.Timer
-
-	// Set the timer for routines about the min mark.
-	if id > w.minRoutines {
-		timer = time.NewTimer(w.idleTime)
-		idle = timer.C
-	}
-
-done:
-	for {
-		select {
-		case t := <-w.tasks:
-			atomic.AddInt64(&w.active, 1)
-			{
-				// Perform the work.
-				t.Work(id)
-			}
-			atomic.AddInt64(&w.active, -1)
-
-		case <-w.kill:
-			break done
-
-		case <-idle:
-			break done
-		}
-
-		// If this goroutine can die on idle time
-		// then reset the timer.
-		if timer != nil {
-			timer.Reset(w.idleTime)
-		}
-	}
-
-	// Decrement the counts.
-	atomic.AddInt64(&w.routines, -1)
-	w.wg.Done()
-
-	log.Println("Work : gr : Info : Shutdown")
-}
-
-// Run wait for the goroutine pool to take the work
-// to be executed.
-func (w *Work) Run(work Worker) {
-	atomic.AddInt64(&w.pending, 1)
-	{
-		w.tasks <- work
-	}
-	atomic.AddInt64(&w.pending, -1)
-}
-
-// Shutdown waits for all the workers to finish.
-func (w *Work) Shutdown() {
-	close(w.shutdown)
-	w.wg.Wait()
 }
